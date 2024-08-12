4a03aa78c1b4976da38019815ba22ae9215df242
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-12 14:17:08 +0200

    Solved all core exercises

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -52,11 +52,20 @@ public class Exercise extends ExerciseBase {
         provided to the name and age members
      */
 
+    public Exercise(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
 
 
     /*
         2. Create a method named add that accepts two integers. The method should return the numbers added together.
      */
+
+    public int add(int first, int second){
+        return first + second;
+    }
 
 
 
@@ -64,6 +73,9 @@ public class Exercise extends ExerciseBase {
         3. Create another method named add that accepts two Strings. The method should return the strings concatenated
         together with a space in between.
      */
+    public String add(String first, String second) {
+        return first + " " + second;
+    }
 
 
 
